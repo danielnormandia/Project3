a499a306a7d29237927f3c10e1b49ce0eafc8089
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2016-12-19 13:40:18 -0500

    change spotify login redirect to users#show

--- app_architecture/config/routes.rb
+++ app_architecture/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'users#index'
-  get '/auth/spotify/callback', to: 'users#spotify'
+  get '/auth/spotify/callback', to: 'users#show'
 
   resources :users, :moods, :comments
 
