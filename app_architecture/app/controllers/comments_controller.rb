224369ce41cd79118ba7b361f919189b0969aa6b
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2016-12-19 15:53:58 -0500

    patch comments form and functionality

--- app_architecture/app/controllers/comments_controller.rb
+++ app_architecture/app/controllers/comments_controller.rb
@@ -8,8 +8,17 @@ class CommentsController < ApplicationController
   end
 
   def create
-
-  end
+   comment = params['comment']
+   user = params['user']
+   mood = params['mood']
+   Comment.create(content: comment['content'],
+                created_at: Time.now,
+                updated_at: Time.now,
+                artist_name: comment['artist_name'],
+                track_name: comment['track_name'],
+                user_id: comment['user_id'],
+                mood_id: )
+ end
 
 
 end
