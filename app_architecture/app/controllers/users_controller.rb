577439a67968fde8d21624d65c861299f6d359cb
Justin Johnson <justinaugustejohnson@gmail.com>
2016-12-19 15:07:59 -0500

    login saved to db

--- app_architecture/app/controllers/users_controller.rb
+++ app_architecture/app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
   def show
     # This is the dashboard
-    @user = RSpotify::User.new(request.env['omniauth.auth'])
+    spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    @user = spotify_user
+    User.create(display_name: spotify_user.display_name,
+                email: spotify_user.email,
+                country: spotify_user.country,
+                followers: spotify_user.followers,
+                images: spotify_user.images,
+                credentials: spotify_user.credentials,
+                created_at: Time.now)
   end
 end
