037b584e03af4b992392c39a013371bb86f0db41
Justin Johnson <justinaugustejohnson@gmail.com>
2016-12-20 15:49:33 -0500

    COMMENTS WORK TOO NOW! YAY

--- app_architecture/db/migrate/20161220193647_change_comment_user_id_to_string.rb
+++
@@ -1,5 +0,0 @@
-class ChangeCommentUserIdToString < ActiveRecord::Migration[5.0]
-  def change
-    change_column :comments, :user_id,  :string
-  end
-end
--- app_architecture/db/schema.rb
+++ app_architecture/db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161220193647) do
+ActiveRecord::Schema.define(version: 20161219192307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.string   "content"
-    t.string   "user_id"
+    t.integer  "user_id"
     t.string   "mood_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
