13136e419b9e66ce4107f26554ba74b129532707
Daniel Normandia <danielalbertnormandia@gmail.com>
2016-12-21 11:39:12 -0500

    clean sessions controller

--- app_architecture/app/controllers/sessions_controller.rb
+++ app_architecture/app/controllers/sessions_controller.rb
@@ -2,18 +2,11 @@ class SessionsController < ApplicationController
   def create
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
     @user = spotify_user
-    @userEmail = User.find_by email: "#{@user.email}"
-    p "@user:"
-    p @user
-    p "@userEmail:"
-    p @userEmail
-    localUser = @user;
-    localUserEmail = @userEmail;
-
-    if ((localUser.email) == (localUserEmail.email))
-      p "then"
+    @userInDb = User.find_by email: "#{@user.email}"
+    sessionUser = @user;
+    emailCheck = @userInDb;
+    if ((sessionUsers.email) == (emailCheck.email))
     else
-      p "else"
     User.create(display_name: spotify_user.display_name,
                 email: spotify_user.email,
                 country: spotify_user.country,
