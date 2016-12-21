62a6c156d70b0808ef22f45b87f22bfe37f0f56f
Justin Johnson <justinaugustejohnson@gmail.com>
2016-12-21 17:13:25 -0500

    still working on playlists

--- app_architecture/app/controllers/application_controller.rb
+++ app_architecture/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :spotify_search
+  helper_method :spotify_search, :playlist_create, :playlist_add
 
   def spotify_search(track_name, artist_name)
     track_name = track_name.gsub(" ", "+")
@@ -11,13 +11,20 @@ class ApplicationController < ActionController::Base
     uri
   end
 
-  def playlist_add(track_name, artist_name)
-    track_name = track_name.gsub(" ", "+")
-    artist_name = track_name.gsub(" ", "+")
-    response = HTTParty.get("https://api.spotify.com/v1/search?q=#{track_name}+#{artist_name}&type=track,artist")
-    track = response['tracks']['items'][0]
-
+  def playlist_create
     current_user = session[:spotify_user]
+    spotify_user = RSpotify::User.new(current_user)
+    playlist = spotify_user.create_playlist!('Test Playlist')
+  end
+
+  def playlist_add
+    comments = Mood.find_by(id: params[:id]).comments
+    tracks = Array.new
+    comments.each do |comment|
+      response = HTTParty.get("https://api.spotify.com/v1/search?q=#{comment.track_name}+#{comment.artist_name}&type=track,artist")
+      tracks.push(response)
+    end
+    playlist.add_tracks!(tracks)
 
   end
 
