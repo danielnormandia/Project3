4de308b970d00b6e182375899bd122f35acef1c3
Daniel Normandia <danielalbertnormandia@gmail.com>
2016-12-22 11:09:23 -0500

    remove current user moods from moods show

--- app_architecture/app/controllers/moods_controller.rb
+++ app_architecture/app/controllers/moods_controller.rb
@@ -1,12 +1,14 @@
 class MoodsController < ApplicationController
 
   def index
-    @moods = Mood.all
+    @moods = Mood.where.not(user_id: "#{User.find_by(email: session[:spotify_user]["email"]).id}")
   end
+
   def show
     @mood = Mood.find_by(id: params[:id])
     @comments = Mood.find_by(id: params[:id]).comments
   end
+
   def create
     #add mood to db
     mood = params['mood']
@@ -16,6 +18,7 @@ class MoodsController < ApplicationController
                  updated_at: Time.now,
                  user_id: mood['user_id'])
   end
+
   def update
     mood = params['mood']
     Mood.create(content: mood['content'],
